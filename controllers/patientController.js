001a4d0309cb331052c0f9399abdf68de7ae0f95
isanka <maduwanthaisanka@gmail.com>
2026-08-03 17:18:17 +0530

    Add Function to Show Patient Upcoming Appointments

--- controllers/patientController.js
+++ controllers/patientController.js
@@ -1,8 +1,28 @@
-async function getPatientById(req,res){
-    const {patientId} = req.body;
-    
-}
-async function getPatientHistory(req,res){
+const Appointments = require('../models/Appointments');
+
+// @ts-ignore
+async function getUpcomingAppointments(req, res) {
+    try {
+        const patientid = req.user.userId;
+        const upAppointments = await Appointments.find({
+            patientId: patientid
+        })
+
+        return res.status(200).json({
+            Success: true,
+            message: "Upcoming Appointments ",
+            upcomingAppointments: upAppointments
+        })
+        console.log(patientid)
+    } catch (error) {
+        res.json({
+            "error": error
+        })
+    }
+
 
 }
-module.exports = {getPatientById,getPatientHistory}
+async function getPatientHistory(req, res) {
+
+}
+module.exports = { getUpcomingAppointments, getPatientHistory }
--- routes/patientRoutes.js
+++ routes/patientRoutes.js
@@ -1,21 +1,19 @@
 const express = require('express');
-const { getPatientById,getPatientHistory } = require('../controllers/patientController');
+const { getUpcomingAppointments,getPatientHistory } = require('../controllers/patientController');
 const Router = express.Router()
 const { authenticateToken } = require('../middleware/auth')
-Router.get('/',authenticateToken,(req,res)=> {
-    res.send("These Are patients List");
-})
-Router.get('/:id',getPatientById);
+Router.get('/get-appointments',authenticateToken,getUpcomingAppointments)
+// Router.get('/:id',getPatientById);
 
-Router.get('/:id/history',getPatientHistory)
-Router.get('/:id/appointments',(req,res)=> {
+// Router.get('/:id/history',getPatientHistory)
+// Router.get('/:id/appointments',(req,res)=> {
   
-    res.send(req.params.id + "'s Appointments");
-})
-Router.get('/:id/treatments',(req,res)=> {
-    res.send(req.params.id + "'s Treatments");
-})
-Router.get('/:id/invoices',(req,res)=> {
-    res.send(req.params.id + "'s invoices");
-})
+//     res.send(req.params.id + "'s Appointments");
+// })
+// Router.get('/:id/treatments',(req,res)=> {
+//     res.send(req.params.id + "'s Treatments");
+// })
+// Router.get('/:id/invoices',(req,res)=> {
+//     res.send(req.params.id + "'s invoices");
+// })
 module.exports = Router;
