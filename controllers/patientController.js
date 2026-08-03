9e4a78c1bad4d031660516866890100bb6aa82ca
isanka <maduwanthaisanka@gmail.com>
2026-08-03 17:49:30 +0530

    Add Appoitment Cancel Function

--- controllers/patientController.js
+++ controllers/patientController.js
@@ -22,7 +22,39 @@ async function getUpcomingAppointments(req, res) {
 
 
 }
+//  @ts-ignore
+async function cancelAppointment(req,res){
+    try{
+        const userId = req.user.userId;
+        console.log(userId);
+        const { _id } = req.body;
+        const appointment = await Appointments.findOne({
+            _id: _id,
+            patientId: userId,
+            status: 'BOOKED'
+        })
+        console.log(appointment)
+        if(!appointment){
+            return res.status(404).json({
+                success:false,
+                message: "Appointment not found , already cancelled, or unauthorized"
+            })
+        }
+        
+        appointment.status = "CANCELLED";
+        const savedAppointment = await appointment.save();
+        console.log(savedAppointment);
+
+       return res.status(200).json({
+        success: true,
+        message: "Appointment Cancelled Successfully"
+       })
+
+    } catch (error) {
+        console.log(error)
+    }
+}
 async function getPatientHistory(req, res) {
 
 }
-module.exports = { getUpcomingAppointments, getPatientHistory }
+module.exports = { getUpcomingAppointments,cancelAppointment, getPatientHistory }
--- routes/patientRoutes.js
+++ routes/patientRoutes.js
@@ -1,9 +1,9 @@
 const express = require('express');
-const { getUpcomingAppointments,getPatientHistory } = require('../controllers/patientController');
+const { getUpcomingAppointments,cancelAppointment,getPatientHistory } = require('../controllers/patientController');
 const Router = express.Router()
 const { authenticateToken } = require('../middleware/auth')
 Router.get('/get-appointments',authenticateToken,getUpcomingAppointments)
-// Router.get('/:id',getPatientById);
+Router.post('/cancel-appointment',authenticateToken,cancelAppointment);
 
 // Router.get('/:id/history',getPatientHistory)
 // Router.get('/:id/appointments',(req,res)=> {
