9a97f9d9ab6d836118d0f74977ebab4d85025f07
isanka <maduwanthaisanka@gmail.com>
2026-08-04 12:50:48 +0530

    add error status code to another line

--- helpers/appointmentValidator.js
+++ helpers/appointmentValidator.js
@@ -2,15 +2,17 @@ const Appointments = require('../models/Appointments');
 const DoctorLeave = require('../models/DoctorLeave');
 const WorkingHours = require('../models/WorkingHours')
 const { getDateName } = require('./getDateName')
+// @ts-ignore
 const { parseTimeToDate, formatHHMM } = require('../helpers/timeDateFunctions')
 // @ts-ignore
 exports.validateAppointmentSlot = async (appointmentDate, startTime) => {
 
-
     // check Date and Time Exists
     if (!appointmentDate || !startTime) {
+        const error = new Error('All Appointment Data is Required');
         // @ts-ignore
-        throw new Error('All Appointment Data is Required').statusCode(400);
+        error.statusCode = 400;
+        throw error;
     }
 
     // Check Appointment Time 
@@ -18,7 +20,10 @@ exports.validateAppointmentSlot = async (appointmentDate, startTime) => {
     const currentTime = new Date();
 
     if (bookingDate < currentTime) {
-        throw new Error('Cannot Book appointments in past');
+        const error = new Error('Cannot Book appointments in past');
+        // @ts-ignore
+        error.statusCode = 400;
+        throw error;
     }
 
     // Check Clinic Closed
@@ -26,13 +31,19 @@ exports.validateAppointmentSlot = async (appointmentDate, startTime) => {
     const schedule = await WorkingHours.findOne({ dayOfWeek: dayName });
     if (!schedule) {
         // @ts-ignore
-        throw new Error(`Clinic Closed on ${dayName}`).statusCode = 400;
+        const error = new Error(`Clinic Closed on ${dayName} `);
+        // @ts-ignore
+        error.statusCode = 400;
+        throw error;
     }
 
     const hour = parseInt(startTime.split(':')[0]);
     if (hour < 9 || hour > 17) {
         // @ts-ignore
-        throw new Error("Appointment Time Must be Within Clinic Hours(9AM - 17PM) ").statusCode(400)
+        const error = new Error("Appointment Time Must be Within Clinic Hours(9AM - 17PM) ");
+        // @ts-ignore
+        error.statusCode = 400;
+        throw error;
     }
 
     // Check Appointment Already Exist
@@ -42,15 +53,22 @@ exports.validateAppointmentSlot = async (appointmentDate, startTime) => {
     });
     if (appointment) {
         // @ts-ignore
-        throw new Error("Appointment Exist At The Selected Time Choose Another").statusCode(400);
+        console.log("Appointment Exist At The Selected Time Choose Another")
+        const error = new Error("Appointment Exist At The Selected Time Choose Another");
+        // @ts-ignore
+        error.statusCode = 400;
+        throw error;
     }
 
     // Check if Doctor is in Leave
     const leaves = await DoctorLeave.findOne({ leaveDate: appointmentDate });
     if (leaves) {
         // @ts-ignore
-        throw new Error("Docter will be in Leave").statusCode(400);
+        const error = new Error("Docter will be in Leave");
+        // @ts-ignore
+        error.statusCode = 400;
+        throw error;
     }
-    
+
     return true;
 }
